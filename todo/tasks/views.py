2d5c936e27d0d5115b7c2d6487033746f739b452
sabuhi.isazade@yahoo.com <sabuhi.isazade@yahoo.com>
2023-01-26 12:27:29 +0100

    template

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hi, I am Sabuhi. Nice to meet you!!!")
+    return render(request, 'tasks/list.html')
